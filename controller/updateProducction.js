6bc04025cd3062f9d2e3b4c93b9f157a8e389fb0
Agustt123 <agustintracheskyoficial@gmail.com>
2025-08-27 12:47:03 -0300

    dsada

--- controller/updateProducction.js
+++ controller/updateProducction.js
@@ -11,7 +11,7 @@ const updateProducction = async (jsonData) => {
     quien,
     latitud,
     longitud,
-    desde,
+    desde = "",
   } = jsonData;
   let dbConnection;
   console.log(jsonData, "jsonData");
@@ -81,8 +81,8 @@ const updateProducction = async (jsonData) => {
       }
 
       const sqlInsertHistorial = `
-           INSERT INTO envios_historial (didEnvio, estado, quien, fecha, didCadete,latitud,longitud,desde) 
-           VALUES (?, ?, ?, ?, ?,?,?,?)
+           INSERT INTO envios_historial (didEnvio, estado, quien, fecha, didCadete,latitud,longitud) 
+           VALUES (?, ?, ?, ?, ?,?,?)
        `;
 
       const resultado = await executeQuery(dbConnection, sqlInsertHistorial, [
@@ -93,7 +93,6 @@ const updateProducction = async (jsonData) => {
         didCadete,
         lat,
         long,
-        desde
       ]);
       console.log(estado);
       return resultado.insertId;
